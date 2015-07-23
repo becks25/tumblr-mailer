2c724d089aa976ba666320c367167760287a63cd
Beckylee <Beckylee.Dell@tufts.edu>
2015-07-23 17:25:27 -0400

    successfully sent email :D

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -8,6 +8,8 @@
 var fs = require('fs');
 var ejs = require('ejs');
 var tumblr = require('tumblr.js');
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('mQFem5V7VHLJjjRyoxkaeg');
 
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
 var email = fs.readFileSync('email_template.html').toString();
@@ -17,7 +19,7 @@ var parsed_data = csvParse(csvFile);
 
 var today = Date.now();
 var oneWeek = 604800000;
-var oneWeekAgo = today;
+var oneWeekAgo = today - oneWeek;
 var completeEmails = [];
 
 //parse through the contact list
@@ -46,7 +48,7 @@ client.posts('beckyleedell.tumblr.com', function (err, data) {
 	var posts = [];
 	for(var i = 0; i < data.posts.length; i++){
 		var curr_post = data.posts[i];
-		if(curr_post.timestamp <= oneWeekAgo){
+		if(curr_post.timestamp >= oneWeekAgo){
 			posts.push({
 				'href': curr_post.post_url,
 				'title': curr_post.title
@@ -70,9 +72,39 @@ client.posts('beckyleedell.tumblr.com', function (err, data) {
 		});
 	}
 
-	console.log(completeEmails);
+	for(var i = 0; i<completeEmails.length; i++){
+		sendEmail(completeEmails[i].to_name, completeEmails[i].to_email, 'Beckylee', 'dr.oreo@gmail.com','Test Subject', completeEmails[i].message_html);
+	}
 
 });
 
 
-
+  function sendEmail(to_name, to_email, from_name, from_email, subject, message_html){
+    var message = {
+        "html": message_html,
+        "subject": subject,
+        "from_email": from_email,
+        "from_name": from_name,
+        "to": [{
+                "email": to_email,
+                "name": to_name
+            }],
+        "important": false,
+        "track_opens": true,    
+        "auto_html": false,
+        "preserve_recipients": true,
+        "merge": false,
+        "tags": [
+            "Fullstack_Tumblrmailer_Workshop"
+        ]    
+    };
+    var async = false;
+    var ip_pool = "Main Pool";
+    mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+   
+    }, function(e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
+ }
